5bd4bb16de3ce776bd6fb5db94052ac7dd898724
pjo901018 <pjo901018@gmail.com>
2017-08-25 17:06:24 +0900

    Spinner binding

--- app/src/main/java/com/example/jaeyoung/smartsseminar02_databinding/MainActivity.java
+++ app/src/main/java/com/example/jaeyoung/smartsseminar02_databinding/MainActivity.java
@@ -8,9 +8,12 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 
 import com.example.jaeyoung.smartsseminar02_databinding.databinding.ActivityMainBinding;
 import com.example.jaeyoung.smartsseminar02_databinding.viewmodel.MainViewModel;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +24,14 @@ public class MainActivity extends AppCompatActivity {
         MainViewModel mainViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
         binding.setMainviewmodel(mainViewModel);
 
+
+        binding.spinnerCompanies.setAdapter(
+                new ArrayAdapter<String>(
+                        this,
+                        R.layout.support_simple_spinner_dropdown_item,
+                        new ArrayList<String>()
+                )
+        );
     }
 
 }
--- app/src/main/java/com/example/jaeyoung/smartsseminar02_databinding/viewmodel/MainViewModel.java
+++ app/src/main/java/com/example/jaeyoung/smartsseminar02_databinding/viewmodel/MainViewModel.java
@@ -6,6 +6,9 @@ import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by jaeyoung on 25/08/2017.
  */
@@ -15,6 +18,9 @@ public class MainViewModel extends ViewModel {
     public ObservableField<String> mTitle = new ObservableField<>("initial");
     @NonNull
     public ObservableField<Integer> mProgressbarStatus = new ObservableField<>(View.GONE);
+    @NonNull
+    public ObservableField<Integer> mSpinnerVisibility = new ObservableField<>(View.VISIBLE);
+    public ObservableField<List<String>> mCompanies = new ObservableField<>();
 
     public MainViewModel() {
         Handler handler = new Handler();
@@ -25,6 +31,12 @@ public class MainViewModel extends ViewModel {
             public void run() {
                 mProgressbarStatus.set(View.GONE);
                 mTitle.set("데이터로딩완료");
+
+                ArrayList<String> companies = new ArrayList<String>();
+                companies.add("smArts");
+                companies.add("samsung");
+
+                mCompanies.set(companies);
             }
         }, 5000);
     }
