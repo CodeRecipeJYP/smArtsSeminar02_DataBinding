d2714f674b7869800397f2678ca5365ae010bffb
pjo901018 <pjo901018@gmail.com>
2017-08-25 16:50:06 +0900

    Data loading view에 적용 완료

--- app/src/main/java/com/example/jaeyoung/smartsseminar02_databinding/viewmodel/MainViewModel.java
+++ app/src/main/java/com/example/jaeyoung/smartsseminar02_databinding/viewmodel/MainViewModel.java
@@ -2,6 +2,7 @@ package com.example.jaeyoung.smartsseminar02_databinding.viewmodel;
 
 import android.arch.lifecycle.ViewModel;
 import android.databinding.ObservableField;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.view.View;
 
@@ -14,4 +15,17 @@ public class MainViewModel extends ViewModel {
     public ObservableField<String> mTitle = new ObservableField<>("initial");
     @NonNull
     public ObservableField<Integer> mProgressbarStatus = new ObservableField<>(View.GONE);
+
+    public MainViewModel() {
+        Handler handler = new Handler();
+
+        mProgressbarStatus.set(View.VISIBLE);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mProgressbarStatus.set(View.GONE);
+                mTitle.set("데이터로딩완료");
+            }
+        }, 5000);
+    }
 }
